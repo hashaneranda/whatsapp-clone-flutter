9678f75669f23e4685b17ddff7c7d139a8c87bd8
Hashan Eranda <hashaneranda@gmail.com>
2018-08-26 00:34:34 +0530

    Initial commit

---
+++ chatapp/lib/models/chat_model.dart
@@ -0,0 +1,73 @@
+
+
+class ChatModel{
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name,this.message,this.time,this.avatarUrl});
+
+
+  
+  
+  
+  }
+
+  List<ChatModel> dummyData = [
+    new ChatModel(
+      name: "David Bekahm",
+      message: "hey Flutter , Ur amazing",
+      time: "10.50",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "Messi",
+      message: "Fluuter using WhatsApp",
+      time: "14.20",
+      avatarUrl: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "Amy",
+      message: "Flutter is so cool",
+      time: "11.10",
+      avatarUrl: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "Sean",
+      message: "Flutter is amazing",
+      time: "16.50",
+      avatarUrl: "https://images.pexels.com/photos/213117/pexels-photo-213117.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "David Bekahm",
+      message: "hey Flutter , Ur amazing",
+      time: "10.50",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "David Bekahm",
+      message: "hey Flutter , Ur amazing",
+      time: "10.50",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "David Bekahm",
+      message: "hey Flutter , Ur amazing",
+      time: "10.50",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "David Bekahm",
+      message: "hey Flutter , Ur amazing",
+      time: "10.50",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+    new ChatModel(
+      name: "David Bekahm",
+      message: "hey Flutter , Ur amazing",
+      time: "10.50",
+      avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+    ),
+
+  ];
